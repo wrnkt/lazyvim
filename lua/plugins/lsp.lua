7a8e39af598f3b06a8116f17d1fc9b64fb8e7d80
Niko Ware <wrnkt1@gmail.com>
2024-02-15 01:11:20 -0500

    Update null-ls config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -70,13 +70,15 @@ return {
   },
   {
     "nvimtools/none-ls.nvim",
+    lazy = false,
     opts = function(_, opts)
-      local none_ls = require("none-ls")
+      local none_ls = require("null-ls")
       opts.sources = vim.list_extend(opts.sources, {
         none_ls.builtins.formatting.clang_format.with({
           extra_args = {
             "-style=file:" .. vim.fn.expand("~/.clang-format"),
           },
+          filetype = { "c", "cpp", "java" },
         }),
       })
     end,
