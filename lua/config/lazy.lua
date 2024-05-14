1217bf3c168e9a5d7983a724848842d71092f854
Niko Ware <wrnkt1@gmail.com>
2024-05-14 19:03:30 -0400

    Add formatter extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
     -- { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
